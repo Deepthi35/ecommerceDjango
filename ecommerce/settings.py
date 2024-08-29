0f4391bcbc64fa7b3a3fc1d4450f21c0eeddf863
Deepthi35 <deepthimadhabattula@gmail.com>
2024-08-29 16:25:31 +0530

    trying to host in vercel

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,28 +11,28 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
-import dj_database_url 
+
 from pathlib import Path
 
 from dotenv import load_dotenv 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR/".eVar", ".env" ))
+# load_dotenv(os.path.join(BASE_DIR/".eVar", ".env" ))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-xrw)_)29%*_#6rvdiz$#ngr=)x1z42r7s1#ayp3r)63)y=ee28"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS").split()
+# ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS").split()
 # Application definition
 
 INSTALLED_APPS = [
